4787be6de6b96d9866c25b49b08b8191254bddea
Saranya <jsaran.bj@gmail.com>
2024-06-12 13:54:09 +0200

    Create database and migrate

--- table_booking/settings.py
+++ table_booking/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,13 +79,17 @@ WSGI_APPLICATION = 'table_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {
+    'default':dj_database_url.parse(os.environ.get
+    ("DATABASE_URL","postgres://u2xzgnmh6kh:OjzZIPIrzk37@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/scoot_yeast_click_149773"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
