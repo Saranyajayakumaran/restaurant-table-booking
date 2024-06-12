1cc9021d66e8e18d27cca3578b021bc000e79335
Saranya <jsaran.bj@gmail.com>
2024-06-12 07:48:00 +0200

    Create view and get respeonse in webpage

--- table_booking/settings.py
+++ table_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@t(w65mp8g&9lmtjo9+-ez(*z8z%vav^*la&g%%2!+f9k8%x64
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
