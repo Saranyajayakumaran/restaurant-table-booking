6db69f4fbaba6e6c1c63aada0cbb2a873b5c871d
Saranya <jsaran.bj@gmail.com>
2024-06-12 15:10:29 +0200

    Add new secret key

--- table_booking/settings.py
+++ table_booking/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("aa6449f3ded65e4065fbe85da92a50b761427c004f69cc6b53")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
