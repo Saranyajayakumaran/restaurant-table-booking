14549ab2f53769177d1d91685234598dde3bc6c3
Saranya <jsaran.bj@gmail.com>
2024-06-12 14:04:41 +0200

    Configure for deployment

--- table_booking/settings.py
+++ table_booking/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@t(w65mp8g&9lmtjo9+-ez(*z8z%vav^*la&g%%2!+f9k8%x64'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
