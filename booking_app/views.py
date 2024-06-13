a08072b91641f71d6854a789608fa34541830603
Saranya <jsaran.bj@gmail.com>
2024-06-13 14:59:38 +0200

    Create a view for menupage models

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import HomePageContent
-from.models import ContactInformation
+from .models import ContactInformation
+from .models import MenuPageContent
 
 # Create your views here.
 def homepage(request):
-    content=HomePageContent.objects.first()
-    contact_details=ContactInformation.objects.first()
+    content=HomePageContent.objects.all()
+    contact_details=ContactInformation.objects.all()
     return render(request,'home.html',{'content':content,'contact_details':contact_details})
+
+def menupage(request):
+    content=MenuPageContent.objects.all()
+    return render(request,'menu.html',{'content':content})
--- table_booking/urls.py
+++ table_booking/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from booking_app.views import homepage
+from booking_app.views import menupage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homepage,name='home')
+    path('',homepage,name='home'),
+    path('menu/',menupage,name='menu')
 ]
