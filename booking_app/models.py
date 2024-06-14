8cd495ef3ced8b8e90bae41f016b964afd79652c
Saranya <jsaran.bj@gmail.com>
2024-06-14 12:11:46 +0200

    Create a model for table booking

--- booking_app/models.py
+++ booking_app/models.py
@@ -12,7 +12,7 @@ class HomePageContent(models.Model):
 
 class ContactInformation(models.Model):
     address=models.CharField(max_length=200)
-    phone_number=models.CharField(max_length=30)
+    phone_number=models.CharField(max_length=15)
     email=models.EmailField()
     opening_hours=models.TextField()
 
@@ -26,3 +26,17 @@ class MenuPageContent(models.Model):
 
     def __str__(self):
         return self.name
+ 
+class Booking(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=30)
+    date=models.DateField()
+    time=models.TimeField()
+    number_of_guest=models.IntegerField()
+    phone_number = models.CharField(max_length=20)
+    email=models.EmailField()
+    special_requests=models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f"Booking for {self.user.username} on {self.date} at {self.time} is confirmed"
+
