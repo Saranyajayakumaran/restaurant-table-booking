7631304abc5aba1ea4e121f18c5b7cd7d18cd16e
Saranya <jsaran.bj@gmail.com>
2024-07-15 19:52:50 +0200

    connected Database neon postgresql

--- table_booking/settings.py
+++ table_booking/settings.py
@@ -92,17 +92,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #Database
 #https://docs.djangoproject.com/en/4.2/ref/settings/databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
-    #'default':dj_database_url.parse(os.environ.get
-    #("DATABASE_URL")),
+    #'default': {
+       #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 #}
+
+DATABASES = {
+    'default':dj_database_url.parse(os.environ.get
+    ("DATABASE_URL")),
+}
 
 CSRF_TRUSTED_ORIGINS = [
     "https://http://127.0.0.1:8000/",
